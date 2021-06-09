cbe737704ddb8e20dc639e3ee81fb823c366da0f
Rusiru Abhisheak <rusiruavb98@gmail.com>
2021-06-09 18:34:47 +0530

    Implemented GitHub API feature

---
+++ documentation/github.api.md
@@ -0,0 +1,10 @@
+### GitHub API 
+I have used GitHub developer API to get my code collaboration information to the application.
+
+#### Get your GitHub profile information
+**GET** <https://api.github.com/users/{your_github_username}> \
+Using this API call you can get user profile data from the GitHub API. Replace `{your_github_username}` with your username.
+
+#### Get your Repository infromation
+**GET** <https://api.github.com/users/{your_github_username}/repos> \
+Using this API call you can get user repository data from the GitHub API. Replace `{your_github_username}` with your username.
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Express } from "express";
 import { createUser, loginUser, getUserAccount, getPublicUserAccount, updateUser} from './controller/user.controller';
+import { updateGitHubProfile, updateRepositories, getGitHubProfile, getGitHubRepositories } from './controller/github.controller';
 import auth from './middleware/auth.middleware';
 
 export default function (app: Express) {
@@ -7,5 +8,9 @@ export default function (app: Express) {
   app.post('/user/login', loginUser);
   app.get('/user/', auth, getUserAccount);
   app.get('/user/public', getPublicUserAccount);
-  app.put('/user/update', auth, updateUser)
+  app.put('/user/update', auth, updateUser);
+  app.put('/user/github/profile/update', auth, updateGitHubProfile);
+  app.put('/user/github/repos/update', auth, updateRepositories);
+  app.get('/user/github/profile', getGitHubProfile);
+  app.get('/user/github/repos', getGitHubRepositories);
 }
